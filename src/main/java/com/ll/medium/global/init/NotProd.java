fc76a28b5f5739eff78acaf7ccd9e28602856bee
taehoon <xogns2134@gmail.com>
2024-01-03 20:34:21 +0900

    notProd add 100

--- src/main/java/com/ll/medium/global/init/NotProd.java
+++ src/main/java/com/ll/medium/global/init/NotProd.java
@@ -25,17 +25,17 @@ public class NotProd {
             if (memberService.getMemberCount() == 2) {
                 memberService.join("user1", "user1", "1234", false);
                 memberService.join("user2", "user2", "1234", false);
-                IntStream.rangeClosed(5, 6).forEach(i -> {
+                IntStream.rangeClosed(5, 104).forEach(i -> {
                     memberService.join("user" + i, "user" + i, "1234", true);
                 });
             }
 
 
             if (postService.getPostCount() == 0) {
-                IntStream.rangeClosed(1, 2).forEach(i -> {
+                IntStream.rangeClosed(1, 50).forEach(i -> {
                     postService.createPost(memberService.findByUserId(3).get(), "제목 " + i, "내용 " + i, true, false);
                 });
-                IntStream.rangeClosed(3, 4).forEach(i -> {
+                IntStream.rangeClosed(51, 150).forEach(i -> {
                     postService.createPost(memberService.findByUserId(3).get(), "제목 " + i, "내용 " + i, true, true);
                 });
             }
